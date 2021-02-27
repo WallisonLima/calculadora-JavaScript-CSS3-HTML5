58428c172639880cf7bcbd25491b4a7bd027b5b8
WallisonLima <wallisonlimaoliveira@gmail.com>
2021-02-27 10:07:53 -0300

    criando servidor com Express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());      
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
+
+app.listen(process.env.PORT || 3000,() => {
+    console.log(`Servidor rodando ${process.env.PORT || 3000}`);
+});
